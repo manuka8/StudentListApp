7755db3213f61058c17b0a06a7b0182d10d857c6
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2025-01-15 14:29:20 +0530

    update packeges

--- studentApp/App.js
+++ studentApp/App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
 export default function App() {
+  const Stack = createNativeStackNavigator
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator intialRouteName='StudentList'screenOptions={
+          {
+            headerStyle: { backgroundColor: '#4b0150' },
+            headerTintColor:'#ffff', 
+            headerTitleAlign:'center'
+        }}>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
