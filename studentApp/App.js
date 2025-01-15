314b5122258b32ee5b929a2837382e67cf03a759
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2025-01-15 21:51:35 +0530

    update App.js using new routes

--- studentApp/App.js
+++ studentApp/App.js
@@ -4,17 +4,19 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
+import StudentList from './components/StudentList';
 export default function App() {
-  const Stack = createNativeStackNavigator
+  const Stack = createNativeStackNavigator();
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <Stack.Navigator intialRouteName='StudentList'screenOptions={
+        <Stack.Navigator initialRouteName='StudentList'screenOptions={
           {
             headerStyle: { backgroundColor: '#4b0150' },
             headerTintColor:'#ffff', 
             headerTitleAlign:'center'
         }}>
+          <Stack.Screen name='StudentList' component={StudentList} options={{ title: "UoV Health Care" }} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
