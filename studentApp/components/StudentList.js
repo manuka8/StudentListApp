a410e972a3f8e4df7c796730579cc60b2c04861f
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2025-01-16 09:09:29 +0530

    Modify StudentList.js file

--- studentApp/components/StudentList.js
+++ studentApp/components/StudentList.js
@@ -2,20 +2,23 @@ import { FlatList, TouchableOpacity, View } from "react-native";
 import { students } from "./StudentsDb";
 import { Button, Card, Text } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
-
-export default function StudentList() {
+import { useState } from "react";
+import React from 'react';
+export default function StudentList({route}) {
   const navigation = useNavigation();
+  const [mystudents,setStudent] = useState(students);
+  const [rerender,setRerender]=useState(false);
   React.useEffect(() => {
-    if (route.params?.post) {
-      // Post updated, do something with `route.params.post`
-      // For example, send the post to the server
-      alert('New post: ' + route.params?.post);
+    if (route.params?.newStudent) {
+      const {newStudent} = route.params;
+      setStudent((prev)=>[...prev,{...newStudent}]);
+      setRerender(!rerender);
     }
-  }, [route.params?.post]);
+  }, [route.params?.newStudent]);
   return (
     <View>
       <FlatList
-        data={students}
+        data={mystudents}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <TouchableOpacity
