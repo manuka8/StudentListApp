990fe2770c510f9a90f070bbc24f6067c6d1aa0c
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2025-01-16 08:24:33 +0530

    update AddStudent.js page

--- studentApp/components/AddStudent.js
+++ studentApp/components/AddStudent.js
@@ -1,3 +1,31 @@
-export default function AddStudent(){
+import { View } from "react-native";
+import { Button, TextInput } from "react-native-paper";
+import { useState } from "react";
+
+export default function AddStudent() {
+  const [name, setName] = useState("");
+  const newStudent = {
+    id: 21,
+    name: name,
+    age: 21,
+    address: "123 Maple Street, Springfield",
+    email: "alice.johnson@example.com",
     
+  } 
+  return (
+    <View>
+      <TextInput
+        label="Email"
+        value={name}
+        onChangeText={setName}
+        mode="outlined"
+      />
+
+      <Button
+        onPress={() => {
+          navigation.popTo("StudentList", { newstudent: text });
+        }}
+      ></Button>
+    </View>
+  );
 }
