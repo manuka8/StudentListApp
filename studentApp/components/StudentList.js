bd077ce6c5dc9fe072b4d333b94c5182dff583ca
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2025-01-16 07:36:25 +0530

    create button

--- studentApp/components/StudentList.js
+++ studentApp/components/StudentList.js
@@ -1,19 +1,38 @@
 import { FlatList, TouchableOpacity, View } from "react-native";
 import { students } from "./StudentsDb";
-import { Card, Text } from "react-native-paper";
+import { Button, Card, Text } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
 
-export default function StudentList(){
-    const navigation = useNavigation();
-    return(
-    <View >
-        <FlatList
+export default function StudentList() {
+  const navigation = useNavigation();
+  return (
+    <View>
+      <FlatList
         data={students}
-        keyExtractor={(item)=>item.id}
-        renderItem={({item})=><TouchableOpacity onPress={()=>navigation.navigate('Profile', {
-            student:item
-          })}><Card><Text>{item.name}</Text></Card></TouchableOpacity>} 
-        />
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate("Profile", {
+                student: item,
+              })
+            }
+          >
+            <Card>
+              <Text>{item.name}</Text>
+            </Card>
+          </TouchableOpacity>
+        )}
+      />
+      <Button
+        
+        mode="contained"
+        onPress={() =>{
+            navigation.navigate("AddStudent")
+        }}
+      >
+        ADD STUDENT
+      </Button>
     </View>
-    )
+  );
 }
